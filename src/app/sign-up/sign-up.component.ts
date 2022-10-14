36976065d3c10d0896a52ffa778c26ddd3896462
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-14 16:27:19 +0530

    routes

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -8,7 +8,36 @@ import { Component, OnInit } from '@angular/core';
 export class SignUpComponent implements OnInit {
 
   constructor() { }
-
+  Name=""
+  PhoneNo=""
+  Gender=""
+  EmailId=""
+  Pincode=""
+  Address=""
+  BloodGroup=""
+  dob=""
+  Age=""
+  ParentName=""
+  Username=""
+  Password=""
+readRegister=()=>{
+  let data={
+    "Name":this.Name,
+    "PhoneNo":this.PhoneNo,
+    "Gender":this.Gender,
+    "EmailId":this.EmailId,
+    "Pincode":this.Pincode,
+    "Address":this.Address,
+    "BloodGroup":this.BloodGroup,
+    "dob":this.dob,
+    "Age":this.Age,
+    "ParentName":this.ParentName,
+    "Username":this.Username,
+    "Password":this.Password
+  }
+  console.log(data)
+  alert("Successfully added")
+}
   ngOnInit(): void {
   }
 
